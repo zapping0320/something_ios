94e9542282686c81dfe4a6cbf72dd7b421d5bcc6
John DongHyun Kim <zapping0320@gmail.com>
2017-11-04 21:27:16 +0900

    Initial Commit

---
+++ something/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  something
+//
+//  Created by 김동현 on 2017. 11. 4..
+//  Copyright © 2017년 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
