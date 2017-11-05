131e74a8e6356e845316d3673f4dd09f11276655
John DongHyun Kim <zapping0320@gmail.com>
2017-11-05 22:06:32 +0900

    - add 5 view controller classes - delete sample view controllers

---
+++ something/ViewController/NoteViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NoteViewController.swift
+//  something
+//
+//  Created by 김동현 on 2017. 11. 5..
+//  Copyright © 2017년 John Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NoteViewController : UIViewController {
+    
+}
---
+++ something/ViewController/NotebookViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NotebookViewController.swift
+//  something
+//
+//  Created by 김동현 on 2017. 11. 5..
+//  Copyright © 2017년 John Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotebookViewController : UIViewController {
+    
+}
