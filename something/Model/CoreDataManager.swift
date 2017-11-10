652cd905c6c1a888e3ee9313ca30469ad7bf23a2
John DongHyun Kim <zapping0320@gmail.com>
2017-11-10 23:29:08 +0900

    testring coredata save

---
+++ something/Model/CoreDataManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataManager.swift
+//  something
+//
+//  Created by 김동현 on 2017. 11. 10..
+//  Copyright © 2017년 John Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class CoreDataManager:NSObject {
+    /*
+    private class func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.
+    }*/
+}
--- something/ViewController/AddNotebookViewController.swift
+++ something/ViewController/AddNotebookViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class AddNotebookViewController : UIViewController {
     
@@ -30,7 +31,36 @@ class AddNotebookViewController : UIViewController {
             return
         }
         
-        //todo check duplicagted
+        //todo check duplicated
+        
+        //add new notebook
+        
+            guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                    return
+            }
+            
+            // 1
+        /*
+            let managedContext = (UIApplication.shared.delegate as? AppDelegate).persistentContainer.viewContext
+            // 2
+            let entity = NSEntityDescription.entity(forEntityName: "Notebook",
+                                           in: managedContext)!
+            
+            let notebook = NSManagedObject(entity: entity,
+                                         insertInto: managedContext)
+            
+            // 3
+            notebook.setValue(tf_name_outlet.text, forKeyPath: "name")
+            notebook.setValue(Date(), forKeyPath: "created_at")
+            
+            // 4
+            do {
+                try managedContext.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        */
     }
     
 }
