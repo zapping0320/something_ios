4242a0bbf767019507126c2cc7a9b264db82e095
John DongHyun Kim <zapping0320@gmail.com>
2018-05-03 20:49:46 +0900

    2 search view에서 전체 노트가 나오게 한다 2-1 2에서 보이는 노트 중 검색된 결과가 포함된 결과가 나오게 한다.

--- something/ViewController/SearchViewController.swift
+++ something/ViewController/SearchViewController.swift
@@ -11,11 +11,12 @@ import UIKit
 import CoreData
 import RealmSwift
 
-class SearchViewController : UIViewController, UITableViewDelegate, UITableViewDataSource  {
+class SearchViewController : UIViewController, UITableViewDelegate, UITableViewDataSource ,UISearchBarDelegate {
     
+    @IBOutlet weak var sb_searchBar: UISearchBar!
     @IBOutlet weak var tableview: UITableView!
     
-    fileprivate var searchednotesarray:[R_Note] = [R_Note]()
+   fileprivate var searchednotesarray:[R_Note] = [R_Note]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +25,15 @@ class SearchViewController : UIViewController, UITableViewDelegate, UITableViewD
         self.tableview.delegate = self
         self.tableview.dataSource = self
         
+        self.sb_searchBar.delegate = self
+        
         //loadSearchedNotes()
     }
     override func viewDidAppear(_ animated: Bool) {
-        loadSearchedNotes()
+        //loadSearchedNotes()
     }
     
-    func loadSearchedNotes() {
+    func loadSearchedNotes(keyword:String) {
         searchednotesarray = [R_Note]()
         
         let realm = try! Realm()
@@ -38,39 +41,22 @@ class SearchViewController : UIViewController, UITableViewDelegate, UITableViewD
         print(results.count)
         for i in 0..<results.count {
             let item = results[i]
-            searchednotesarray.append(item)
+           if(item.content.contains(keyword)
+            || item.title.contains(keyword)
+            || keyword == "")
+           {
+                searchednotesarray.append(item)
+            }
         }
         
-        /*
-        DispatchQueue(label: "background").async {
-            autoreleasepool {
-                let realm = try! Realm()
-                let theDog = realm.objects(Dog.self).filter("age == 1").first
-                try! realm.write {
-                    theDog!.age = 3
-                }
-            }
-        }*/
-        
-        /*
-        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
-        do {
-            // 결과값 담기
-            let searchResults = try CoreDataManager.shared.getContext().fetch(fetchRequest)
-            // for 문을 이용하여 Key 값에 대한 Value 값 가져오기
-            for note in searchResults as [NSManagedObject] {
-                let value = String(describing: note.value(forKey: "title"))
-                print("\(value)")
-                let newitem = NoteItem()
-                newitem.title = note.value(forKey: "title") as! String
-                searchednotesarray.append(newitem)
-            }
-        } catch {
-            print("Error with request: \(error)")
-        }*/
+       
         
         
         self.tableview.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        loadSearchedNotes(keyword: searchText)
     }
 }
 
