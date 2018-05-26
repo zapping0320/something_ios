9e2cba3b9229dcfacad4613c7445b72b896bf664
John DongHyun Kim <zapping0320@gmail.com>
2018-05-27 08:14:10 +0900

    노트 제목/내용 수정 구현

--- something/ViewController/NoteViewController.swift
+++ something/ViewController/NoteViewController.swift
@@ -27,7 +27,7 @@ class NoteViewController : UIViewController {
         
         let realm = try! Realm()
         let notebooks = realm.objects(R_Notebook.self)
-        let selectedNotebook = notebooks[self.selectedNotebookindex]
+        let selectedNotebook = notebooks[notebooks.count - self.selectedNotebookindex - 1]
         var targetNotes = [R_Note]()
         let notes = realm.objects(R_Note.self)
         for i in 0..<notes.count {
@@ -42,12 +42,36 @@ class NoteViewController : UIViewController {
         let dateformatter = DateFormatter()
         dateformatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
         self.label_updated_at.text = dateformatter.string(from: selectedNote.updated_at)
+        self.switch_favorite.isOn = selectedNote.isfavorite
         self.tf_ol_title.text = selectedNote.title
         self.tv_ol_content.text = selectedNote.content
  
     }
     
     @IBAction func btn_SaveNote(_ sender: Any) {
+        if(self.tf_ol_title.text == "" && self.tv_ol_content.text == "")
+        {
+            let alert = UIAlertController(title: title,
+                                          message: "you entered no text, please check",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let realm = try! Realm()
+        
+        try! realm.write {
+            selectedNote.title = self.tf_ol_title.text!
+            selectedNote.content = self.tv_ol_content.text
+            selectedNote.isfavorite = self.switch_favorite.isOn
+            selectedNote.updated_at = Date()
+        }
+        
         self.dismiss(animated: true, completion: nil)
     }
 }
